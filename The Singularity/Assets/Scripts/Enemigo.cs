0c56dc035fc7943073889d87b56559f70776f340
Dxvah <idayraperezdelgado@gmail.com>
2024-06-07 19:44:27 +0100

    no se ahora no funcionan las cosas
    :)

--- The Singularity/Assets/Scripts/Enemigo.cs
+++ The Singularity/Assets/Scripts/Enemigo.cs
@@ -6,29 +6,58 @@ using UnityEngine.AI;
 public class Enemigo : MonoBehaviour
 {
     public GameObject player;
-    
     public NavMeshAgent enemy;
+    public Collider enemyCol;
+    public Animator enemyAnim;
+    private bool isBeingWatched = false;
 
-    private void Start()
+    void Start()
     {
         enemy = GetComponent<NavMeshAgent>();
-        player = GameObject.Find("Player");            
+        enemyCol = GetComponent<Collider>();
+        player = GameObject.Find("Player");
     }
+
     void Update()
     {
-       
-        enemy.SetDestination(player.transform.position);
+        if (!isBeingWatched)
+        {
+            enemy.SetDestination(player.transform.position);
+        }
     }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag ("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             PauseGame();
         }
     }
+
     void PauseGame()
     {
-
         Time.timeScale = 0;
     }
+
+    void RestartGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void SetBeingWatched(bool isWatched)
+    {
+        isBeingWatched = isWatched;
+        if (isWatched)
+        {
+            enemyAnim.enabled = false;
+        }
+        else
+        {
+            enemyAnim.enabled = true;
+        }
+        Debug.Log("El enemigo está siendo observado: " + isWatched);
+    }
 }
+
+
+
--- The Singularity/Assets/Scripts/RayCastCamera.cs
+++ The Singularity/Assets/Scripts/RayCastCamera.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class RayCastCamera : MonoBehaviour
 {
-    public Image mira; 
-    public Camera cam; 
+    public Image mira;
+    public Camera cam;
+    public float raycastDistance = 100f;
+    public LayerMask enemyLayerMask;
+    private Enemigo enemigo;
 
     void Start()
     {
@@ -24,23 +27,40 @@ public class RayCastCamera : MonoBehaviour
 
     void Update()
     {
-        if (mira == null || cam == null)
+        if (mira == null || cam == null || enemigo == null)
         {
-            return; 
+            return;
         }
 
-        
         mira.color = new Color(mira.color.r, mira.color.g, mira.color.b, 0.4f);
 
-        
-        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        Ray ray = cam.ScreenPointToRay(cam.transform.position);
         RaycastHit hit;
-
         
         if (Physics.Raycast(ray, out hit))
         {
             mira.color = new Color(mira.color.r, mira.color.g, mira.color.b, 1f);
-            Debug.Log("Elemento encontrado: " + hit.collider.name);
+            Debug.Log("Elemento encontrado:");
+Debug.Log(hit);
+            Enemigo enemyCol = hit.collider.GetComponent<Enemigo>();
+            if (enemyCol != null)
+            {
+                enemyCol.SetBeingWatched(true);
+            }
+            else
+            {
+                enemyCol.SetBeingWatched(false);
+            }
         }
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(cam.transform.position, transform.forward);
     }
 }
+
+
+
+
